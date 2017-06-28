2eb032444d26037ac8bddce316816ab0ec51749c
James Glover <james.glover@sanger.ac.uk>
2017-06-28 12:39:26 +0100

    Add missing components

---
+++ app/javascript/custom-pooled-tubes/components/NewPool.vue
@@ -0,0 +1,25 @@
+<template>
+  <li><draggable :options="{group:{ name:'wells', put:false}}" class="new-pool" @add="onAdd" :list="wells">+ New tube</draggable></li>
+</template>
+
+<script>
+
+import Draggable from 'vuedraggable'
+
+export default {
+  name: 'NewPool',
+  data () {
+    return {
+      wells: []
+    }
+  },
+  components: {
+    draggable: Draggable
+  },
+  methods: {
+    onAdd: function(index, element) {
+      console.log(index);
+    }
+  }
+}
+</script>
---
+++ app/javascript/custom-pooled-tubes/components/Pool.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <draggable :options="{group:'wells'}" :element="'tr'" class="pool"  :list="wells">
+      <td v-for="well in wells"><well v-bind="well" :label="true"></well></td>
+    </draggable>
+  </li>
+</template>
+
+<script>
+
+
+import Well from '../../shared_components/Well.vue'
+import Draggable from 'vuedraggable'
+
+export default {
+  name: 'Pool',
+  data () {
+    return {
+      wells: [{location:'H12',state:'failed'}]
+    }
+  },
+  components: {
+    well: Well,
+    draggable: Draggable
+  }
+}
+</script>
+
